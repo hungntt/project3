4b3ee51bd70c619364e4a69f8dbfa1758a845565
hungntt <minatosnow@gmail.com>
2023-04-10 15:38:58 +0200

    Finish project + Add screenshots

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,16 +1,17 @@
-import os
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy 
-from azure.servicebus import QueueClient
-
+from azure.servicebus import ServiceBusClient
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 
 app.secret_key = app.config.get('SECRET_KEY')
 
-queue_client = QueueClient.from_connection_string(app.config.get('SERVICE_BUS_CONNECTION_STRING'),
-                                                 app.config.get('SERVICE_BUS_QUEUE_NAME'))
+# queue_client = QueueClient.from_connection_string(app.config.get('SERVICE_BUS_CONNECTION_STRING'),
+#                                                  app.config.get('SERVICE_BUS_QUEUE_NAME'))
+service_bus_client = ServiceBusClient.from_connection_string(conn_str=app.config.get('SERVICE_BUS_CONNECTION_STRING'),
+                                                             logging_enable=True)
+queue_client = service_bus_client.get_queue_sender(queue_name=app.config.get('SERVICE_BUS_QUEUE_NAME'))
 
 db = SQLAlchemy(app)
 
