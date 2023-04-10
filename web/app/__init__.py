37b47d241f5217763be55f311712bfc93985c157
hungntt <minatosnow@gmail.com>
2023-04-10 15:55:09 +0200

    Reformat

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -7,8 +7,6 @@ app.config.from_object('config.DevelopmentConfig')
 
 app.secret_key = app.config.get('SECRET_KEY')
 
-# queue_client = QueueClient.from_connection_string(app.config.get('SERVICE_BUS_CONNECTION_STRING'),
-#                                                  app.config.get('SERVICE_BUS_QUEUE_NAME'))
 service_bus_client = ServiceBusClient.from_connection_string(conn_str=app.config.get('SERVICE_BUS_CONNECTION_STRING'),
                                                              logging_enable=True)
 queue_client = service_bus_client.get_queue_sender(queue_name=app.config.get('SERVICE_BUS_QUEUE_NAME'))
